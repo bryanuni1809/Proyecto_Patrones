32378d340033931f864788c942ff310c64aa5197
admin <andreadelr.lc@gmail.com>
2026-05-18 09:41:25 -0500

    avance

---
+++ Proyecto_Patrones/Proyecto_Patrones/src/main/java/Abstractas/Pokemon.java
@@ -0,0 +1,21 @@
+package Abstractas;
+
+public abstract class Pokemon {
+    protected String nombre;
+    protected String tipo;
+    protected int defensa;
+    protected int ataque;
+    protected int vida;
+
+    public Pokemon(String nombre, String tipo, int defensa, int ataque, int vida) {
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.defensa = defensa;
+        this.ataque = ataque;
+        this.vida = vida;
+    }
+    
+    public abstract void ataqueComun();
+
+
+}
---
+++ Proyecto_Patrones/Proyecto_Patrones/src/main/java/com/mycompany/proyecto_patrones/Proyecto_Patrones.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto_patrones;
+
+import Pokemones.psiquico.Abra;
+
+/**
+ *
+ * @author admin
+ */
+public class Proyecto_Patrones {
+
+    public static void main(String[] args) {
+        Abra adra = new Abra("Adra", 0, 0, 0);
+        System.out.println(adra);
+    }
+}
